735ef22cd5146444ee6c2932e302d3b417bb276c
Ramez Dekhil <ramezdekhil@gmail.com>
2023-05-26 17:58:31 +0300

    Card

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ function App() {
     base:`" nav " " main"`, 
     lg:`" nav nav" "aside main"`
   }} >
-    <GridItem area="nav " >
+    <GridItem area="nav" >
       <NavBar/>
       </GridItem>
     <Show above ="lg"> 
-    <GridItem area="aside " >Aside</GridItem>
+    <GridItem area="aside" >Aside</GridItem>
     </Show>
     <GridItem area="main " >
       <GameGrid></GameGrid>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,23 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
-import {Text} from "@chakra-ui/react"
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
-interface Game {
-    id:number;
-    name:string;
-}
-interface FetchGamesResponse {
-    count : number;
-    results: Game[];
-}
 const GameGrid = () => {
- const [games,setGames]=useState<Game[]>([]);
- const [error , setError] = useState('');
- useEffect(() => {
-    apiClient.get<FetchGamesResponse>('/games')
-    .then(res=>setGames(res.data.results))
-    .catch(err=>setError(err.message));
- }
- )
+  const { games, error } = useGames();
   return (
     <>
-    {error && <Text>{error}</Text>}
-    <ul>
-       {
-        games.map(games => <li key={games.id}>{games.name}</li>)
-       } 
-    </ul>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3 }}
+      padding="10px"
+      spacing={6}
+    > 
+        {games.map((game) => (
+          <GameCard key={game.id} game={game}/>
+        ))}
+      </SimpleGrid>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
