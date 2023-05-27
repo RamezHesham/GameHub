34d7e0afb81571915d415ed70ab433fb23d1739f
Ramez Dekhil <ramezdekhil@gmail.com>
2023-05-27 20:37:21 +0300

    Filtering completed

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,11 @@
 import { HStack, List, ListItem ,Image,Text, Spinner, Button } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
 
-const GenreList = () => {
+}
+const GenreList = ({onSelectGenre}:Props) => {
   const { data ,isLoading,error} = useGenres();
   if(error) return null;
   if(isLoading) return <Spinner/>;
@@ -18,7 +21,7 @@ const GenreList = () => {
                 objectFit="cover"
                 src={genre.image_background}
               />
-           <Button onClick={() =>console.log(genre)} fontSize='lg' variant='link'>
+           <Button onClick={() =>onSelectGenre(genre)} fontSize='lg' variant='link'>
             {genre.name}
            </Button>
           
