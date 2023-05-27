d3c20720f7aba1f1172563cf4ebb90acf37ec620
Ramez Dekhil <ramezdekhil@gmail.com>
2023-05-27 22:01:22 +0300

    Highlighting selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,9 +3,9 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-
+  selectedGenre: Genre | null;
 }
-const GenreList = ({onSelectGenre}:Props) => {
+const GenreList = ({onSelectGenre,selectedGenre}:Props) => {
   const { data ,isLoading,error} = useGenres();
   if(error) return null;
   if(isLoading) return <Spinner/>;
@@ -21,7 +21,7 @@ const GenreList = ({onSelectGenre}:Props) => {
                 objectFit="cover"
                 src={genre.image_background}
               />
-           <Button onClick={() =>onSelectGenre(genre)} fontSize='lg' variant='link'>
+           <Button fontWeight={genre.id===selectedGenre?.id ? 'bold': 'normal'} onClick={() =>onSelectGenre(genre)} fontSize='lg' variant='link'>
             {genre.name}
            </Button>
           
